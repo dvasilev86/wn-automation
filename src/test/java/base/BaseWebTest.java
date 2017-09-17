9a09dea212c20febb6d35b2d5af2d224063ef3d4
Deyan Vasilev <deyanvasilev86@gmail.com>
2017-09-17 15:14:28 +0300

    making it possible to run webtests with different browser

--- src/test/java/base/BaseWebTest.java
+++ src/test/java/base/BaseWebTest.java
@@ -20,7 +20,21 @@ public class BaseWebTest {
 
     @Before
     public void Init() throws MalformedURLException {
-        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        DesiredCapabilities capability;
+
+            if (System.getProperty("browser")!= null && (System.getProperty("browser").contentEquals("chrome"))) {
+            capability = DesiredCapabilities.chrome();
+
+        }
+
+        else if (System.getProperty("browser")!= null && (System.getProperty("browser").contentEquals("firefox"))) {
+            capability = DesiredCapabilities.firefox();
+        }
+        else {
+                System.out.print("WARNING! SESSION IS RUNNING ON CHROME BECAUSE NO SYSTEM PROPERTY IS SET" +
+                        "THIS IS TO MAKE SURE YOU CAN RUN TESTS FROM JAVA IDE WITHOUT CALLING GRADLE");
+                capability = DesiredCapabilities.chrome();
+            }
 
         driver = new RemoteWebDriver(new URL(Config.HUB_URL), capability);
         driver.manage().window().maximize();
